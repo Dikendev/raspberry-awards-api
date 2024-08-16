3f65208c16006fc260b164ef125bf42b56f760c4
Dikendev <diken.dev@gmail.com>
2024-08-16 16:26:30 -0300

    feat: Add endpoint to wipe database

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import mongoose from 'mongoose';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,16 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('wipe')
+  async wipeDataBase() {
+    try {
+      await mongoose.connect(process.env.DATABASE_URL, {});
+      await mongoose.connection.db.dropDatabase();
+      console.log('Database wiped');
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
