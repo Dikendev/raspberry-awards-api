8e37d607743dddb27df76cbdffc40e363cc8dbb2
Dikendev <diken.dev@gmail.com>
2024-08-16 16:17:07 -0300

    Feat: add mongoose dependency / update module with studio / movie / producers imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,24 @@ import { LoggerModule } from './external/logger/infrastructure/logger.module';
 import { ContextModule } from './context/infrastructure/context.module';
 import { ConfigModule } from './config/config.module';
 import { FileParserModule } from './main/domain/file-parser/file-parser.module';
-import { HttpExceptionFilter } from './infrastructure/filter/http-exception.filter';
+import { HttpExceptionFilter } from './infrastructure/exceptions/filter/app/http-exception.filter';
 import { HealthModule } from './health/health.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StudioModule } from './main/domain/entity/studio/studio.module';
+import { MovieModule } from './main/domain/entity/movie/movie.module';
+import { ProducersModule } from './main/domain/entity/producer/producers.module';
 
 @Module({
   imports: [
+    MongooseModule.forRoot(process.env.DATABASE_URL),
     ConfigModule,
     LoggerModule,
     ContextModule,
     FileParserModule,
     HealthModule,
+    StudioModule,
+    MovieModule,
+    ProducersModule,
   ],
   providers: [
     AppService,
