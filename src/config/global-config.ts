56291bee6682a6d465c66f964c932bd8ed5086ac
Dikendev <diken.dev@gmail.com>
2024-08-16 16:10:09 -0300

    Chore refactor main structure methods encapsulating in the global config file

--- src/config/global-config.ts
+++ src/config/global-config.ts
@@ -1,10 +1,52 @@
 import { INestApplication } from '@nestjs/common';
 import { LoggerAdapterService } from '../external/logger/infrastructure/logger-adapter.service';
-import { HttpExceptionFilter } from '../infrastructure/filter/http-exception.filter';
+import { HttpExceptionFilter } from '../infrastructure/exceptions/filter/app/http-exception.filter';
+import { Logger } from '@nestjs/common';
+import { ZodFilter } from '../infrastructure/exceptions/filter/zod/zod.filter';
+import { MongoExceptionFilter } from '../infrastructure/exceptions/filter/mongo/mongo-exception.filter';
 
-export function globalConfig(app: INestApplication): void {
+export async function globalConfig(app: INestApplication): Promise<void> {
+  const logger = new Logger('NestApplication');
+  logger.log('Starting application');
+
+  globalFilters(app);
+
   app.useLogger(app.get(LoggerAdapterService));
-  app.useGlobalFilters(new HttpExceptionFilter());
   app.enableShutdownHooks();
   app.enableCors();
+
+  shutDown(app, logger);
+
+  await serverListen(app, logger);
+}
+
+export function globalFilters(app: INestApplication) {
+  app.useGlobalFilters(
+    new HttpExceptionFilter(),
+    new ZodFilter(),
+    new MongoExceptionFilter(),
+  );
+}
+
+async function serverListen(
+  app: INestApplication,
+  logger: Logger,
+): Promise<void> {
+  await app.listen(process.env.PORT);
+  logger.log(`Server running on http://localhost:${process.env.PORT}`);
+}
+
+export async function shutDown(
+  app: INestApplication,
+  logger: Logger,
+): Promise<void> {
+  const cleanup = async () => {
+    await app.close();
+    logger.log('Application closed');
+    process.exit(0);
+  };
+
+  process.on('SIGTERM', cleanup);
+  process.on('SIGINT', cleanup);
+  process.on('exit', cleanup);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
 import { globalConfig } from './config/global-config';
 
-async function bootstrap() {
-  const logger = new Logger('NestApplication');
-  logger.log('Starting application');
+(async () => {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
-
-  const cleanup = async () => {
-    await app.close();
-    logger.log('Application closed');
-    process.exit(0);
-  };
-
-  process.on('SIGTERM', cleanup);
-  process.on('SIGINT', cleanup);
-  process.on('exit', cleanup);
-
   globalConfig(app);
-
-  await app.listen(process.env.PORT);
-  logger.log(`Server running on http://localhost:${process.env.PORT}`);
-}
-bootstrap();
+})();
